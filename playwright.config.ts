f6b9680139fe4c1bac1673f167659b4c0e0071c5
davidh8011 <davidh80@verizon.net>
2023-03-09 15:51:49 -0600

    added cron

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'retain-on-failure',
     video: 'off',
   },
 
